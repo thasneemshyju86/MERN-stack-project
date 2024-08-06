6ef3b60508a33adaa29923ea15fb84d0913d0c6a
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-08-06 11:27:02 -0700

    user registration

---
+++ routes/api/users.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt=require('bcryptjs')
+const gravatar=require('gravatar')
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
+// @route    GET api/users
+// @route    POST api/users
+// @desc     Test route
+// @desc     Register user
+// @access   Public
+router.post(
+	"/",
+	[
+		check("name", "Name is required").not().isEmpty(),
+		check("email", "Please include a valid email").isEmail(),
+		check(
+			"password",
+			"please enter a password with min 6 or more characters"
+		).isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+		// see if user exist
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				res.status(400).json({ errors: [{ msg: "User already exists" }] });
+			}
+			
+
+            //get users gravatar
+            const avatar=gravatar.url(email,{
+                s:'200',
+                r:'pg',
+                d:'mm'
+            })
+             // create an instance of the user
+             user=new User({
+                name,
+                email,
+                avatar,
+                password
+             })
+
+		//encrypt password
+        const salt=await bcrypt.genSalt(10)
+        user.password=await bcrypt.hash(password,salt)
+        await user.save()
+
+		//return jsonwebtoken
+        res.send("User registered");
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+		
+
+		// in order for the req.body to work we need to initialize middleware for the bodyparser.
+	}
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express=require('express');
+const connectDB=require('./config/db')
 
 const app=express();
+// connect Database
+connectDB()
+
+// init middleware (this will allow us to get data in req.body in user.js >routes)
+app.use(express.json({extended:false})) 
 
 app.get('/',(req,res)=>res.send('API running'))
+
+// Define Routes
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/posts'))
+
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>console.log(`server started on ${PORT}`))
