49bd77c16a518c7ccb8db9afa1ea289b2b295ee0
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-08-08 10:54:28 -0700

    middleware & protected route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt=require('bcryptjs')
-const gravatar=require('gravatar')
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -35,34 +37,47 @@ router.post(
 			if (user) {
 				res.status(400).json({ errors: [{ msg: "User already exists" }] });
 			}
-			
 
-            //get users gravatar
-            const avatar=gravatar.url(email,{
-                s:'200',
-                r:'pg',
-                d:'mm'
-            })
-             // create an instance of the user
-             user=new User({
-                name,
-                email,
-                avatar,
-                password
-             })
+			//get users gravatar
+			const avatar = gravatar.url(email, {
+				s: "200",
+				r: "pg",
+				d: "mm",
+			});
+			// create an instance of the user
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
 
-		//encrypt password
-        const salt=await bcrypt.genSalt(10)
-        user.password=await bcrypt.hash(password,salt)
-        await user.save()
+			//encrypt password
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
 
-		//return jsonwebtoken
-        res.send("User registered");
+			//return jsonwebtoken
+			// res.send("User registered");
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+                payload, 
+                config.get("jwtToken"),
+                {expiresIn:36000},
+                (err,token)=>{   //callback
+                    if(err) throw err
+                    res.json({token})
+                }
+            );
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error");
 		}
-		
 
 		// in order for the req.body to work we need to initialize middleware for the bodyparser.
 	}
