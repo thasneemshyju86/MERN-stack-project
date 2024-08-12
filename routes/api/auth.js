4436d39a7886c3417cb2429667f68adef1d9dab1
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-08-11 17:13:38 -0700

    authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express=require('express')
 const router=express.Router()
 const auth=require('../../middleware/auth')
 const User = require('../../models/User')
+const jwt=require('jsonwebtoken')
+const config=require('config')
+const { check, validationResult } = require("express-validator");
+const bcrypt=require('bcryptjs')
 
 // @route   GET api/auth
 // @desc    Test route
@@ -18,6 +22,66 @@ res.json(user)
     res.status(500).send('Server Error')
  }
 })
+
+
+// @route    POST api/auth
+// @desc     Authenticate the user & get token
+// @access   Public
+router.post(
+	"/",
+	
+		check("email", "Please include a valid email").isEmail(),
+		check(
+			"password",
+			"Password required"
+		).exists(),
+	
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+		// see if user exist
+		try {
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+			}
+
+			// compare if the plain text password & encrypted password matches 
+
+            const isMatch=await bcrypt.compare(password,user.password)
+
+            if(!isMatch){
+              return  res.status(400).json({errors:[{msg:"Invalid credentials"}]})
+            }
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+                payload, 
+                config.get("jwtToken"),
+                {expiresIn:36000},
+                (err,token)=>{   //callback
+                    if(err) throw err
+                    res.json({token})
+                }
+            );
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+
+		// in order for the req.body to work we need to initialize middleware for the bodyparser.
+	}
+);
 
 
 module.exports=router
