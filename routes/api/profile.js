b41feeafd2c9758f12fbf975f49b1bb977918961
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-08-19 13:36:22 -0700

    add & delete education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -234,4 +234,102 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
 	}
 });
 
+// @route   PUT api/profile/education
+// @desc    add profile education
+// @access  Private
+
+router.put(
+	"/education",
+	[
+		auth,
+		check("school", "school is required").not().isEmpty(),
+		check("degree", "degree is required").not().isEmpty(),
+		check('fieldofstudy',"field of study required").not().isEmpty(),
+		check("from", "from date is required").not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { school, degree, fieldofstudy, from, to, current, description } =
+			req.body;
+
+		const newEdu = {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.education.unshift(newEdu);
+
+			await profile.save();
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    delete education from profile
+// @access  Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+
+		//Get remove index
+		const removeIndex = profile.education
+			.map((item) => item.id)
+			.indexOf(req.params.edu_id);
+
+		profile.education.splice(removeIndex, 1);
+
+		await profile.save();
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server error");
+	}
+});
+
+// @route 	GET api/profile/github/:username
+// @desc	Get user repos from Github
+// @access	Public
+
+router.get('/github/:username',(req,res)=>{
+	try {
+		const options={
+			uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&
+			client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+			method:'GET',
+			headers:{'user-agent':'node.js'}
+		}
+
+		express.request(options,(error,response,body)=>{
+			if(error) console.error(error)
+
+			if(response.statusCode!=200){
+				return res.status(404).json({msg:'No Github profile found'})
+			}
+
+			res.json(JSON.parse(body))
+		})
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server error')
+		
+	}
+})
+
 module.exports = router;
